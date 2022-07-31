3c395bd702d278f57fc86d1d87a1f5f4f98ba529
likhitha124 <109716931+likhitha124@users.noreply.github.com>
2022-07-31 22:23:20 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,6 @@
+let number = 100;
+let name = "Talentio";
+let flag = true;
+console.log("number is a", typeof(number), "data type.");
+console.log("name is a", typeof(name), "data type.");
+console.log("flag is a", typeof(flag), "data type."); 
